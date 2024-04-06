058679426d8e4b0916d2dd96a34d0d0bfe426c40
Ayush Ranjan <ranaayush0725@gmail.com>
2024-04-06 19:45:25 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import "./App.css";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <div></div>
+      <div>
+        <Navbar />
+        <Hero />
+      </div>
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+const Hero = () => {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold-p-2">
+          Powering positive change with blockchain
+        </p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+          Grow with data
+        </h1>
+        <div>
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+            Fast, flexibke financing for D2C
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
