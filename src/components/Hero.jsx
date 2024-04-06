d918df220ddbaf773f09fcb217e3d95d399bd8f4
Ayush Ranjan <ranaayush0725@gmail.com>
2024-04-06 23:51:45 +0530

    new commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,30 @@
+import { ReactTyped } from "react-typed";
+
 const Hero = () => {
   return (
     <div className="text-white">
-      <div className="max-w-[800px] mt-[-96px] w-full mx-auto text-center flex flex-col justify-center">
-        <p className="text-[#00df9a] font-bold-p-2">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold p-2">
           Powering positive change with blockchain
         </p>
         <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
           Grow with data
         </h1>
-        <div>
-          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
-            Fast, flexibke financing for D2C
+        <div className="flex justify-center items-center">
+          <p className="md:text-3xl sm:text-2xl text-xl font-bold py-4">
+            Fast, flexible financing for
           </p>
+          <ReactTyped
+            className="md:text-3xl sm:text-2xl text-xl font-bold md:pl-4 pl-2"
+            strings={[" SMEs", " Startups", "Fundraisers"]}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
         </div>
+        <p className="md:text-2xl sm:text-xl text-xl font-bold text-gray-500">
+          Secure, transparent fundraising for every cause.
+        </p>
       </div>
     </div>
   );
